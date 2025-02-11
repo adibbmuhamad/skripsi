c8b9c9f782e9b68a25eb241edf5d115938d40774
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2025-02-11 23:01:35 +0700

    Fix(AttendancesSeeder) : fix permission seeder

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -14,10 +14,18 @@ class AttendanceSeeder extends Seeder
         $students = Student::all();
 
         foreach ($students as $student) {
+            // Tentukan status secara acak termasuk 'permission'
+            $statusOptions = ['present', 'absent', 'permission'];
+            $status = $statusOptions[rand(0, 2)]; // Mengacak status antara present, absent, atau permission
+
+            // Tentukan alasan izin (hanya untuk status permission)
+            $permissionReason = $status === 'permission' ? 'Izin Sakit' : null;
+
             Attendance::create([
                 'student_id' => $student->id,
                 'date' => now()->subDays(rand(1, 10)),  // Absensi dalam 10 hari terakhir
-                'status' => rand(0, 1) ? 'present' : 'absent',  // Status hadir atau absen
+                'status' => $status,  // Status hadir, absen, atau izin
+                'permission_reason' => $permissionReason, // Alasan izin jika statusnya izin
             ]);
         }
     }
